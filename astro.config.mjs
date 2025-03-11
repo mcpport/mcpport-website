a22f33093800108626b5cb40afec07259528e275
Kerry <54252779+xxnuo@users.noreply.github.com>
2025-03-11 22:11:23 +0800

    Add daisyui

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import cloudflarePagesHeaders from 'astro-cloudflare-pages-headers';
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
-export default defineConfig({});
-    
+export default defineConfig({
+    integrations: [cloudflarePagesHeaders(), tailwindcss()]
+});
